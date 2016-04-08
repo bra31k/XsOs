8743dd28c4dfdb142ac7b4be0a1c48c282a9b6da
Sergey Patrakeev <bra31k@mail.ru>
2016-04-08 16:09:09 +0500

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -23,6 +23,16 @@ void Cell::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWid
    case State0:
        painter->drawText(25,25, "0");
        break;
+   case StateWinX:
+       painter->setBrush(Qt::green);
+       painter->drawRect(0,0,50,50);
+       painter->drawText(25,25,"X");
+       break;
+   case StateWin0:
+       painter->setBrush(Qt::green);
+       painter->drawRect(0,0,50,50);
+       painter->drawText(25,25, "0");
+       break;
    default:
        break;
    }
@@ -33,25 +43,6 @@ void Cell::mousePressEvent(QGraphicsSceneMouseEvent *event)
     event->accept();
 }
 
-//void Cell::blabla()
-//{
-//    switch(m_state)
-//    {
-//    case Statenothing:
-//        m_state=StateX;
-//        m_text=' ';
-//        break;
-//    case StateX:
-//        m_state=State0;
-//        m_text='x';
-//        break;
-//    case State0:
-//        m_state=StateX;
-//        m_text='0';
-//        break;
-//    }
-
-//}
 void Cell::setState(State stat)
 {
     if (m_state == stat)
